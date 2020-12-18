b32b3b412eed1ef5e98245a2c50bc5412b43d1b5
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2020-12-18 13:03:00 +0100

    Updated Design

--- resources/team/teams.php
+++ resources/team/teams.php
@@ -13,7 +13,7 @@ if (isset($_POST['createTeam'])) {
 <form method="post">
     <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
+            <div class="modal-content" style="max-width: 500px; margin: 1.75rem auto;">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Neues Team erstellen</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
