475f2384e454e687d2347d3218250b6310e113a4
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-02-06 13:10:28 +0100

    Small Change

--- resources/additional/head.php
+++ resources/additional/head.php
@@ -10,6 +10,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= $helper->cdnUrl() ?>images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="author" content="Bastian Leicht">
+
+    <!-- Favicons -->
     <link rel="apple-touch-icon" sizes="57x57" href="<?= $helper->cdnUrl() ?>images/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?= $helper->cdnUrl() ?>images/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="<?= $helper->cdnUrl() ?>images/favicon/apple-icon-72x72.png">
@@ -24,9 +30,6 @@
     <link rel="icon" type="image/png" sizes="96x96" href="<?= $helper->cdnUrl() ?>images/favicon/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?= $helper->cdnUrl() ?>images/favicon/favicon-16x16.png">
     <link rel="manifest" href="<?= $helper->cdnUrl() ?>images/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= $helper->cdnUrl() ?>images/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 
     <title><?= $currPageName; ?> | <?= $helper->siteName(); ?></title>
     <link rel="stylesheet" href="<?= $helper->cdnUrl() ?>vendors/mdi/css/materialdesignicons.min.css">
@@ -40,16 +43,6 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/1063e48ab5.js" crossorigin="anonymous"></script>
 </head>
-
-<?php
-
-if(strpos($currPage,'auth_') !== false) {
-//    echo '<body class="hold-transition login-page">';
-} elseif (strpos($currPage,'back_') !== false) {
-//    echo '<body class="hold-transition sidebar-mini"><div class="wrapper">';
-}
-
-?>
 
 <body>
 <div class="container-scroller">
